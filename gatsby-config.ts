bfc6293d0b2fe9792462255a90dea416c0152d54
m21br <lbr3021@gmail.com>
2023-02-21 15:18:08 +0900

    Page Query

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -10,7 +10,17 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        // The unique name for each instance
+        name: `pages`,
+        // Path to the directory
+        path: `${__dirname}/blog-posts`,
+      },
+    }
+  ],
 }
 
 export default config
