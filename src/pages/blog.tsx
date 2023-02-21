60277ef9658dca41a8c0494add37e9545d63ed86
m21br <lbr3021@gmail.com>
2023-02-21 15:31:49 +0900

    MDX

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -4,24 +4,37 @@ import Layout from "../components/Layout";
 import Seo from "../components/Seo";
 
 
-export default function Blog({ data }: PageProps<Queries.BlogTitlesQuery>){
+export default function Blog({ data }: PageProps<Queries.BlogPostsQuery>){
 	return(
 		<Layout title="Blog">
-      <ul>
-				{data.allFile.nodes.map((file, index) => <li key={index}>{file.name}</li>)}
-			</ul>
+      <section>
+				{data.allMdx.nodes.map((file, index) => 
+				<article key={index}>
+					<h3>{file.frontmatter?.title}</h3>
+					<h5>{file.frontmatter?.author} in: {file.frontmatter?.category}</h5>
+					<h6>{file.frontmatter?.date}</h6>
+					<hr/>
+					<p>{file.excerpt}</p>
+				</article>)}
+			</section>
     </Layout>
 	)
 }
 
 export const query = graphql`
-	query BlogTitles {
-		allFile {
+	query BlogPosts {
+		allMdx {
 			nodes {
-				name
+				frontmatter {
+					author
+					category
+					date
+					title
+				}
+				excerpt(pruneLength: 50)
 			}
-		}
-	}
+  	}
+}
 `
 
 export const Head = () => <Seo title="Blog"></Seo>
