667b30ca8c3cf85064e5e42e81ce6b73c8398e01
m21br <lbr3021@gmail.com>
2023-02-28 13:33:55 +0900

    Blog Post

--- src/pages/blog/{mdx.frontmatter__slug}.tsx
+++ src/pages/blog/{mdx.frontmatter__slug}.tsx
@@ -1,8 +1,35 @@
+import { graphql } from "gatsby";
 import React from "react"
 import Layout from "../../components/Layout";
+import Seo from "../../components/Seo";
 
-export default function BlogPost(){
-	return <Layout title="Blog Post">
-		<div />
-	</Layout>
+interface IBlogPostProps {
+	data: Queries.PostDetailQuery,
+	children: any
 }
+
+export default function BlogPost({data, children} : IBlogPostProps){
+	console.log(data);
+	console.log(children);
+	return (
+		<Layout title="">
+			<div> {children} </div>
+		</Layout>
+	)
+}
+
+export const query = graphql`
+	query PostDetail($frontmatter__slug: String) {
+		mdx(frontmatter: {slug: {eq: $frontmatter__slug}}) {
+			body
+			frontmatter {
+				author
+				category
+				date
+				slug
+				title
+			}
+		}
+	}
+`
+export const Head = ({data}:IBlogPostProps) => <Seo title={data.mdx?.frontmatter?.title!}></Seo>
